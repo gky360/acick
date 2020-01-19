de7e6782488ef0e2f733d96607a8a4d43d8bf2f4
gky360 <gky360@gmail.com>
2020-01-19 12:25:13 +0900

    add default shell

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,9 @@
 use std::collections::hash_map::DefaultHasher;
-use std::fmt;
 use std::hash::{Hash, Hasher as _};
+use std::path::PathBuf;
 use std::process::{Command, Output};
 use std::sync::Mutex;
+use std::{env, fmt};
 
 use anyhow::Context as _;
 use lazy_static::lazy_static;
@@ -50,7 +51,7 @@ pub trait Expand<C: Serialize> {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
-struct Templ(String);
+pub struct Templ(String);
 
 impl<T: ToString> From<T> for Templ {
     fn from(value: T) -> Self {
@@ -131,55 +132,21 @@ impl Expand<ProblemContext> for ProblemTempl {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
-pub struct ShellArray(Vec<String>);
+pub struct TemplArray<T>(Vec<T>);
 
-impl ShellArray {
-    pub fn exec(&self) -> Result<Output> {
-        let output = Command::new(&self.0[0])
-            .args(&self.0[1..])
-            .output()
-            .context(format!("Failed to execute command: \"{}\"", self))?;
-        Ok(output)
+impl<T: Expand<CmdContext>> TemplArray<T> {
+    pub fn expand_all(&self, context: &CmdContext) -> Result<Vec<String>> {
+        self.0.iter().map(|c| c.expand(context)).collect()
     }
 }
 
-impl From<ShellArray> for String {
-    fn from(shell_array: ShellArray) -> String {
-        format!("{}", shell_array)
-    }
-}
-
-impl fmt::Display for ShellArray {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0
-            .iter()
-            .enumerate()
-            .try_for_each(|(i, c)| write!(f, "{}{}", if i == 0 { "" } else { " " }, c))
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
-pub struct ShellTemplArray<T>(Vec<T>);
-
-impl<T: Expand<CmdContext>> ShellTemplArray<T> {
-    pub fn expand_all(&self, context: &CmdContext) -> Result<ShellArray> {
-        let array = self
-            .0
-            .iter()
-            .map(|c| c.expand(context))
-            .collect::<Result<Vec<String>>>()
-            .context("Failed to expand command template")?;
-        Ok(ShellArray(array))
-    }
-}
-
-impl<I, S, T: From<String>> From<I> for ShellTemplArray<T>
+impl<I, S, T: From<String>> From<I> for TemplArray<T>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<str>,
 {
     fn from(value: I) -> Self {
-        ShellTemplArray(
+        TemplArray(
             value
                 .into_iter()
                 .map(|s| s.as_ref().to_string().into())
@@ -188,13 +155,13 @@ where
     }
 }
 
-impl<T: fmt::Display> From<ShellTemplArray<T>> for String {
-    fn from(shell: ShellTemplArray<T>) -> String {
+impl<T: fmt::Display> From<TemplArray<T>> for String {
+    fn from(shell: TemplArray<T>) -> String {
         format!("{}", shell)
     }
 }
 
-impl<T: fmt::Display> fmt::Display for ShellTemplArray<T> {
+impl<T: fmt::Display> fmt::Display for TemplArray<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         self.0
             .iter()
@@ -203,10 +170,57 @@ impl<T: fmt::Display> fmt::Display for ShellTemplArray<T> {
     }
 }
 
+pub type Shell = TemplArray<CmdTempl>;
+
+impl Shell {
+    pub fn exec<T: ToString>(&self, command: T) -> Result<Output> {
+        let cmd_context = CmdContext::new(command);
+        let command = self
+            .expand_all(&cmd_context)
+            .context("Could not expand shell template")?;
+        let output = Command::new(&command[0])
+            .args(&command[1..])
+            .output()
+            .context(format!(
+                "Failed to execute command: \"{}\"",
+                command.join(" ")
+            ))?;
+        Ok(output)
+    }
+}
+
+impl Default for Shell {
+    fn default() -> Self {
+        let env_path = env::var_os("PATH").unwrap_or_default();
+
+        let bash = env::split_paths(&env_path)
+            .chain(if cfg!(windows) {
+                vec![
+                    PathBuf::from(r"C:\tools\msys64\usr\bin"),
+                    PathBuf::from(r"C:\msys64\usr\bin"),
+                    PathBuf::from(r"C:\Program Files\Git\usr\bin"),
+                ]
+            } else {
+                vec![]
+            })
+            .map(|p| {
+                if cfg!(windows) {
+                    p.join("bash").with_extension("exe")
+                } else {
+                    p.join("bash")
+                }
+            })
+            .find(|p| p.exists() && p.to_str().is_some())
+            .unwrap_or_else(|| PathBuf::from("bash"));
+
+        (&[bash.to_str().unwrap(), "-c", "{{ command }}"]).into()
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 #[serde(default)]
 pub struct Config {
-    shell: ShellTemplArray<CmdTempl>,
+    shell: Shell,
     services: ServicesConfig,
 }
 
@@ -220,7 +234,7 @@ impl Config {
 impl Default for Config {
     fn default() -> Self {
         Self {
-            shell: (&["/bin/bash", "-c", "{{ command }}"]).into(),
+            shell: Shell::default(),
             services: ServicesConfig::default(),
         }
     }
@@ -246,8 +260,8 @@ pub struct AtcoderConfig {
     language: String,
     working_directory: ProblemTempl,
     src: ProblemTempl,
-    compile: ShellTemplArray<ProblemTempl>,
-    run: ShellTemplArray<ProblemTempl>,
+    compile: TemplArray<ProblemTempl>,
+    run: TemplArray<ProblemTempl>,
 }
 
 impl Default for AtcoderConfig {
@@ -275,14 +289,6 @@ mod tests {
     }
 
     #[test]
-    fn expand_cmd_templ_arr() -> anyhow::Result<()> {
-        let shell = ShellTemplArray::<CmdTempl>::from(&["/bin/bash", "-c", "{{ command }}"]);
-        let cmd_context = CmdContext::new("echo hello");
-        shell.expand_all(&cmd_context)?;
-        Ok(())
-    }
-
-    #[test]
     fn expand_problem_templ() -> anyhow::Result<()> {
         // let templ = ProblemTempl::from("{{ service.id | snake_case }}/{{ contest.id | kebab_case }}/{{ problem.id | camel_case }}/Main.cpp");
         let templ = ProblemTempl::from("{{ service.id }}");
@@ -299,26 +305,25 @@ mod tests {
     }
 
     #[test]
-    fn expand_cmd_template_arr_failure() -> anyhow::Result<()> {
-        let shell = ShellTemplArray::<CmdTempl>::from(&[
-            "/bin/bash",
-            "-c",
-            "{{ some_undefined_variable }}",
-        ]);
+    fn expand_default_shell() -> anyhow::Result<()> {
+        let shell = Shell::default();
+        let cmd_context = CmdContext::new("echo hello");
+        shell.expand_all(&cmd_context)?;
+        Ok(())
+    }
+
+    #[test]
+    fn expand_shell_failure() -> anyhow::Result<()> {
+        let shell = Shell::from(&["/bin/bash", "-c", "{{ some_undefined_variable }}"]);
         let cmd_context = CmdContext::new("echo hello");
         assert!(shell.expand_all(&cmd_context).is_err());
         Ok(())
     }
 
     #[test]
-    fn exec_shell() -> anyhow::Result<()> {
-        let shell = ShellArray(
-            (&["/bin/bash", "-c", "echo hello"])
-                .iter()
-                .map(|c| (*c).to_string())
-                .collect(),
-        );
-        let output = shell.exec()?;
+    fn exec_default_shell() -> anyhow::Result<()> {
+        let shell = Shell::default();
+        let output = shell.exec("echo hello")?;
         println!("{:?}", output);
         assert!(output.status.success());
         Ok(())
