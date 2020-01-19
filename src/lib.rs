0f00be90ddc091558b02cd4b647f4e4ef6dd352c
gky360 <gky360@gmail.com>
2020-01-20 00:17:30 +0900

    fix opt

--- src/lib.rs
+++ src/lib.rs
@@ -32,32 +32,26 @@ pub enum ServiceKind {
     Atcoder,
 }
 
-#[derive(EnumString, EnumVariantNames, IntoStaticStr, Debug, Clone, PartialEq, Eq, Hash)]
-#[strum(serialize_all = "kebab-case")]
-pub enum LanguageKind {
-    Cpp,
-}
-
 #[derive(StructOpt, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Opt {
     #[structopt(
+        name = "service",
         long,
         global = true,
         env = "ACICK_SERVICE",
         default_value = ServiceKind::Atcoder.into(),
         possible_values = &ServiceKind::VARIANTS,
     )]
-    service: ServiceKind,
-    #[structopt(long, global = true, env = "ACICK_CONTEST", default_value = "abc100")]
-    contest: String,
+    service_id: ServiceKind,
     #[structopt(
+        name = "contest",
         long,
         global = true,
-        env = "ACICK_LANGUAGE",
-        default_value = LanguageKind::Cpp.into(),
-        possible_values = &LanguageKind::VARIANTS
+        env = "ACICK_CONTEST",
+        default_value = "abc100"
     )]
-    language: LanguageKind,
+    contest_id: String,
+
     #[structopt(subcommand)]
     cmd: Cmd,
 }
