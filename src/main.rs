5e56f8087b2ea4a0201c46a9c11f467acdb8eb84
gky360 <gky360@gmail.com>
2020-01-23 11:13:05 +0900

    misc

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ use acick::{Opt, Result};
 fn main() -> Result<()> {
     let opt = Opt::from_args();
     let (stdin, stdout, stderr) = (io::stdin(), io::stdout(), io::stderr());
-    opt.run(stdin.lock(), stdout.lock(), stderr.lock())
-        .map_err(|err| {
-            io::stdout().flush().expect("Could not flush stdout");
-            eprintln!();
-            err
-        })
+    if let Err(err) = opt.run(stdin.lock(), stdout.lock(), stderr.lock()) {
+        io::stdout().flush().expect("Could not flush stdout");
+        eprintln!();
+        return Err(err);
+    }
+    Ok(())
 }
