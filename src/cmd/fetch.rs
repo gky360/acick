f1123a3245f8d749003cbdb11b40258c9e699af8
gky360 <gky360@gmail.com>
2020-02-04 17:11:26 +0900

    fix fetch test

--- src/cmd/fetch.rs
+++ src/cmd/fetch.rs
@@ -24,7 +24,7 @@ impl FetchOpt {
     pub fn default_test() -> Self {
         Self {
             problem_id: None,
-            overwrite: true,
+            overwrite: false,
         }
     }
 }
