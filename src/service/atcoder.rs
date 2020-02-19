9fef99a20ac8c583078095f591e3c91a04b33597
gky360 <gky360@gmail.com>
2020-02-19 18:03:28 +0900

    use rayon to parallize requests to Dropbox

--- src/service/atcoder.rs
+++ src/service/atcoder.rs
@@ -1,13 +1,13 @@
-use std::io;
+use std::io::{self, Write as _};
 
 use anyhow::{anyhow, Context as _};
 use dropbox_sdk::files::FileMetadata;
 use indicatif::{ProgressBar, ProgressStyle};
 use itertools::Itertools as _;
 use maplit::hashmap;
+use rayon::prelude::*;
 use reqwest::blocking::{Client, Response};
 use reqwest::{StatusCode, Url};
-use tokio::stream::{self, Stream as _, StreamExt as _};
 
 use crate::abs_path::AbsPathBuf;
 use crate::config::SessionConfig;
@@ -83,6 +83,8 @@ impl AtcoderActor<'_> {
             "https://www.dropbox.com/sh/arnpe0ef5wds8cv/AAAk_SECQ2Nc6SVGii3rHX6Fa?dl=0";
         static TICK_INTERVAL_MS: u64 = 50;
 
+        writeln!(cnsl, "Downloading testcases from Dropbox ...")?;
+
         let dropbox = DbxAuthorizer::new(
             DBX_APP_KEY,
             DBX_APP_SECRET,
@@ -104,42 +106,32 @@ impl AtcoderActor<'_> {
                 )
             })?;
 
-        // list testcase file path components
-        let components_arr: Vec<Vec<(&Problem, &str, FileMetadata)>> =
-            stream::iter(problems.iter().cartesian_product(&["in", "out"]))
-                .map(|(problem, inout)| {
-                    eprintln!("list_all_files");
-                    dropbox
-                        .list_all_files(
-                            format!("/{}/{}/{}", folder.name, problem.id(), inout),
-                            Some(DBX_TESTCASES_URL),
-                        )
-                        .context("Could not list testcase files on Dropbox")
-                        .map(|files| {
-                            eprintln!("list_all_files_finish");
-                            files
-                                .into_iter()
-                                .map(|file| (problem, *inout, file))
-                                .collect::<Vec<_>>()
-                        })
-                })
-                .collect::<Result<Vec<_>>>()
-                .await?;
-        let components: Vec<(&Problem, &str, FileMetadata)> =
-            components_arr.into_iter().flatten().collect();
-        // for problem in problems.iter() {
-        //     for inout in &["in", "out"] {
-        //         let files = dropbox.list_all_files(
-        //             format!("/{}/{}/{}", folder.name, problem.id(), inout),
-        //             Some(DBX_TESTCASES_URL),
-        //         )?;
-        //         components.extend(files.into_iter().map(|file| (problem, inout, file)));
-        //     }
-        // }
+        // list testcase files
+        let components_arr: Vec<(&Problem, &str, Vec<FileMetadata>)> = problems
+            .iter()
+            .cartesian_product(&["in", "out"])
+            .collect::<Vec<_>>()
+            .into_par_iter()
+            .map(|(problem, inout)| {
+                let files = dropbox
+                    .list_all_files(
+                        format!("/{}/{}/{}", folder.name, problem.id(), inout),
+                        Some(DBX_TESTCASES_URL),
+                    )
+                    .context("Could not list testcase files on Dropbox")?;
+                Ok((problem, *inout, files))
+            })
+            .collect::<Result<Vec<_>>>()?;
+        let components: Vec<(&Problem, &str, FileMetadata)> = components_arr
+            .into_iter()
+            .map(|(problem, inout, files)| {
+                files.into_iter().map(move |file| (problem, inout, file))
+            })
+            .flatten()
+            .collect();
 
         // calculate total size
         let total_size = components.iter().map(|(_, _, file)| file.size).sum();
-        eprintln!("total size: {}", total_size);
 
         // prepare progress bar
         let pb = ProgressBar::with_draw_target(total_size, cnsl.to_pb_target());
@@ -152,22 +144,27 @@ impl AtcoderActor<'_> {
         pb.set_style(style);
         pb.enable_steady_tick(TICK_INTERVAL_MS);
 
-        for (problem, inout, file) in components.into_iter() {
-            pb.set_message(&file.name);
-            let dbx_path = format!("/{}/{}/{}/{}", folder.name, problem.id(), inout, file.name);
-            let mut reader = dropbox.get_shared_link_file(DBX_TESTCASES_URL, dbx_path)?;
-            let abs_path = testcases_path.join(inout).join(file.name);
-            abs_path.save_pretty(
-                |mut file| {
-                    io::copy(&mut reader, &mut file).context("Could not save testcase to file")?;
-                    Ok(())
-                },
-                true,
-                Some(&testcases_path),
-                None,
-            )?;
-            pb.inc(file.size);
-        }
+        // download and save testcase files
+        components
+            .into_par_iter()
+            .try_for_each::<_, Result<()>>(|(problem, inout, file)| {
+                pb.set_message(&file.name);
+                let dbx_path = format!("/{}/{}/{}/{}", folder.name, problem.id(), inout, file.name);
+                let mut reader = dropbox.get_shared_link_file(DBX_TESTCASES_URL, dbx_path)?;
+                let abs_path = testcases_path.join(inout).join(file.name);
+                abs_path.save_pretty(
+                    |mut file| {
+                        io::copy(&mut reader, &mut file)
+                            .context("Could not save testcase to file")?;
+                        Ok(())
+                    },
+                    true,
+                    Some(&testcases_path),
+                    None,
+                )?;
+                pb.inc(file.size);
+                Ok(())
+            })?;
 
         pb.finish();
 
