6174a937969e89274fbb6c288904bd4d27e998e5
gky360 <gky360@gmail.com>
2020-03-22 03:49:44 +0900

    fix clippy error

--- src/service/atcoder.rs
+++ src/service/atcoder.rs
@@ -22,14 +22,18 @@ use crate::service::{Act, ResponseExt as _};
 use crate::web::open_in_browser;
 use crate::{Config, Console, Error, Result};
 
+// TODO: remove allow(clippy::unknown_clippy_lints)
+// when clippy::option_env_unwrap comes into stable
 lazy_static! {
     // Use option_env for builds on crates.io.
     // crates.io does not know these secrets.
     static ref DBX_APP_KEY: &'static str = {
+        #[allow(clippy::unknown_clippy_lints)]
         #[allow(clippy::option_env_unwrap)]
         option_env!("ACICK_DBX_APP_KEY").unwrap()
     };
     static ref DBX_APP_SECRET: &'static str = {
+        #[allow(clippy::unknown_clippy_lints)]
         #[allow(clippy::option_env_unwrap)]
         option_env!("ACICK_DBX_APP_SECRET").unwrap()
     };
