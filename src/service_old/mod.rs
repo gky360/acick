74612c7350ab6a8d95a597dbaf1f2a753b33b9ec
gky360 <gky360@gmail.com>
2020-04-18 23:27:23 +0900

    Remove trait ResponseExt from main crate

--- src/service_old/mod.rs
+++ src/service_old/mod.rs
@@ -1,28 +1,5 @@
-use anyhow::Context as _;
-use reqwest::blocking::Response;
-use reqwest::header::LOCATION;
-use reqwest::Url;
-
-use crate::Result;
-
 pub mod atcoder;
 mod atcoder_full;
 mod atcoder_page;
 
 pub use atcoder::AtcoderActor;
-
-pub trait ResponseExt {
-    fn location_url(&self, base: &Url) -> Result<Url>;
-}
-
-impl ResponseExt for Response {
-    fn location_url(&self, base: &Url) -> Result<Url> {
-        let loc_str = self
-            .headers()
-            .get(LOCATION)
-            .context("Could not find location header in response")?
-            .to_str()?;
-        base.join(loc_str)
-            .context("Could not parse redirection url")
-    }
-}
